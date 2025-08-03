252865340dc12f27074d77fd339f4a6dcdf6d0d2
SURAJ NAGAICH <suraj.nagaich_cs23@gla.ac.in>
2025-08-03 21:35:39 +0530

    Equal sum partitions to Equal sum partitions.java

--- 28-07-2025/Equal sum partitions.java
+++ 28-07-2025/Equal sum partitions.java
@@ -7,13 +7,13 @@ class Solution {
         if(total%2!=0){
             return false;
         }
-        int target = total/2;
+        int target = total/2;        // divide the total by 2 to satisfy the target
         boolean dp[][] = new boolean[nums.length+1][target+1];
         for (int i = 0; i <= nums.length; i++) dp[i][0] = true;
         for(int i=1;i<=nums.length;i++){
             for(int sum = 1;sum<=target;sum++){
                 if(sum<nums[i-1]){
-                    dp[i][sum] = dp[i-1][sum];
+                    dp[i][sum] = dp[i-1][sum];          //by using dp
                 }
                 else{
                     dp[i][sum] = dp[i-1][sum] || dp[i-1][sum-nums[i-1]];
