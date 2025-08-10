6034bb3d85bf9b679136890a28a37fc0e4ea2dab
SURAJ NAGAICH <suraj.nagaich_cs23@gla.ac.in>
2025-08-10 12:34:23 +0530

    Create first recurring character in string.java

---
+++ Week 4/first recurring character in string.java
@@ -0,0 +1,27 @@
+import java.util.HashSet;
+public class FirstRecurringCharacter {
+    public static Character findFirstRecurringCharacter(String s) {
+        HashSet<Character> seen = new HashSet<>();
+        
+        for (char c : s.toCharArray()) {
+            if (seen.contains(c)) {
+                return c;                 // it returns the first recurring character
+            }
+            seen.add(c);
+        }
+        
+        return null;        //  it returns null if no recurring character is found
+    }
+
+    public static void main(String[] args) {
+        String input = "leetcode";
+        Character result = findFirstRecurringCharacter(input);
+        
+        if (result != null) {
+            System.out.println("First recurring character: " + result);
+        } else {
+            System.out.println("No recurring characters found.");
+        }
+    }
+}
+
