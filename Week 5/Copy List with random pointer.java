49adbdb6067aecbd3e43d9ea178e7d56a791bae4
SURAJ NAGAICH <suraj.nagaich_cs23@gla.ac.in>
2025-08-14 11:45:09 +0530

    Copy List with random pointer.java

--- Week 5/Copy List with random pointer.java
+++ Week 5/Copy List with random pointer.java
@@ -7,13 +7,13 @@ class Solution {
             map.put(current, new Node(current.val));
             current = current.next;
         }
-        current = head;
+        current = head;             // initialize the value of head into current
         while (current != null) {
             Node copyNode = map.get(current);
             copyNode.next = map.get(current.next);
             copyNode.random = map.get(current.random);
             current = current.next;
         }
-        return map.get(head);
+        return map.get(head);  // return the element which was found on head position
     }
 }
