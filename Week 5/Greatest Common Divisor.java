2f3488f0847c10eeb3a29d2a487147973aee92fd
SURAJ NAGAICH <suraj.nagaich_cs23@gla.ac.in>
2025-08-17 07:53:13 +0530

    Greatest Common Divisor.java

--- Week 5/Greatest Common Divisor.java
+++ Week 5/Greatest Common Divisor.java
@@ -2,9 +2,9 @@ class Solution {
     public int findGCD(int[] nums) {
         int min = nums[0] , max = nums[0];
         for(int i=0;i<nums.length;i++){
-            if(min > nums[i]){
+            if(min > nums[i]){                           
                 min = nums[i];
-            }
+            }                                              // binary search approach
             if(max < nums[i]){
                 max = nums[i];
             }
@@ -14,7 +14,7 @@ class Solution {
     public int gcd(int[] arr , int a  , int b){
         int min = a;
         while(min > 1){
-            if((a % min == 0) && (b % min == 0)){
+            if((a % min == 0) && (b % min == 0)){          // base case for GCD
                 return min;
             }
             min--;
