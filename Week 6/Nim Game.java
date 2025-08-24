92dfe45f66c6ce0ca10a5ed46d4051175ac83bdb
SURAJ NAGAICH <suraj.nagaich_cs23@gla.ac.in>
2025-08-24 14:06:50 +0530

    Nim Game.java

--- Week 6/Nim Game.java
+++ Week 6/Nim Game.java
@@ -2,6 +2,19 @@
 
 class Solution {
     public boolean canWinNim(int n) {
-        return n % 4 != 0;
+        return n % 4 != 0;      // all the stones can be collected if it is not distributed in group of 4
     }
+}
+
+
+                // Approach 2
+
+class Solution {
+    public boolean canWinNim(int n) {
+               if(n%4==0)
+              {                             // if the number is  completely divisible by y return false
+                return false;
+        }
+        return true;
+}
 }
