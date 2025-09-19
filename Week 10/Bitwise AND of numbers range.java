5e1a9a62cdf334d07e00d08b0d90b63d2168b441
SURAJ NAGAICH <suraj.nagaich_cs23@gla.ac.in>
2025-09-19 07:28:19 +0530

    Bitwise AND of numbers range.java

--- Week 10/Bitwise AND of numbers range.java
+++ Week 10/Bitwise AND of numbers range.java
@@ -2,8 +2,8 @@ class Solution {
     public int rangeBitwiseAnd(int left, int right) {
           int shift = 0;
         while (left < right) {
-            left >>= 1;
-            right >>= 1;
+            left >>= 1;                 // perform left shift
+            right >>= 1;                // perform right shift
             shift++;
         }
         return left << shift;
